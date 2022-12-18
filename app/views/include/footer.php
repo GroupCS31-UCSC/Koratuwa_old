66b34c84da3479b2729beb9996a08452df94460c
Kawya Sandamini <2020cs085@stu.ucsc.cmb.ac.lk>
2022-12-18 13:23:05 +0530

    Change livestock frontend

--- app/views/include/footer.php
+++ app/views/include/footer.php
@@ -82,9 +82,9 @@ var myChart= new Chart(totProfit, {
 
 </script>
 
-<!--charts - Dashboard page -->
+<!--charts - Livestock Dashboard page -->
 <script>
-var profit= document.getElementById('cattle').getContext('2d');
+var cattle= document.getElementById('cattle').getContext('2d');
 var myChart= new Chart(cattle, {
   type: 'polarArea',
   data: {
@@ -97,6 +97,32 @@ var myChart= new Chart(cattle, {
         'rgba(75, 192, 192)',
         'rgba(255, 205, 86)',
         'rgba(201, 203, 207)',
+      ],
+      borderWidth:0.5
+    }]
+  },
+  options: {
+    responsive: true,
+
+  }
+});
+
+
+var pCattle= document.getElementById('pCattle').getContext('2d');
+var myChart= new Chart(pCattle, {
+  type: 'doughnut',
+  data: {
+    labels: ['Pregnant', 'Milking Cattle'],
+    datasets: [{
+      label: 'Milking Cattle Precentages',
+      data: [90, 10],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',,
+        'rgba(255, 206, 86, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(255, 206, 86, 1)',
       ],
       borderWidth:0.5
     }]
